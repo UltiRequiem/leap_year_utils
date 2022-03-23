ca80519b4c3f407ab11b28c361f7759f537ddd40
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-23 07:47:06 -0500

    :recycle: Update

--- scripts/node_support.ts
+++ scripts/node_support.ts
@@ -1,7 +1,8 @@
 import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.8/node_support.ts";
 
 buildPackage({
-  repoName: "leap-year-utils",
+  repoName: "leap_year_utils",
+  name: "leap-year-utils",
   description: "Utilities for working with Leap Years",
   homepage: "https://leap-year-utils.js.org",
   keywords: ["leap", "year", "date"],
