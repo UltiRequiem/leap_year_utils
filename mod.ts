5968c0a67dcdde4857c613700f7be8c30fc46a7d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-20 19:29:56 -0500

    :memo: Update

--- mod.ts
+++ mod.ts
@@ -1,14 +1,15 @@
 import { isLeapYear } from "https://deno.land/x/leap_year@v2.1.0/mod.ts";
 import { parseDateLike } from "https://deno.land/x/parse_datelike@0.0.1/mod.ts";
 
-export function leapYearsInRange(
-  config: {
-    min?: number;
-    max?: Date | number | string;
-  } = {},
-) {
-  const { min = 0, max = new Date() } = config;
+export interface LeapYearsInRangeOptions {
+  min?: number;
+  max?: Date | number | string;
+}
 
+export function leapYearsInRange({
+  min = 0,
+  max = 0,
+}: LeapYearsInRangeOptions) {
   const maxYear = parseDateLike(max);
 
   const leapYears = [];
